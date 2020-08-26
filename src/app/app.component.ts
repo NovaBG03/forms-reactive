d62a208d168123b5aeb02f176e33cca98a33f9f2
Nikita Koynov <nikicha03@gmail.com>
2020-08-26 12:51:21 +0300

    async validator"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -16,7 +17,9 @@ export class AppComponent implements OnInit {
     this.singUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.nameValidator.bind(this)]),
-        'email': new FormControl(null, [Validators.email, Validators.required])
+        'email': new FormControl(null,
+          [Validators.email, Validators.required],
+          [this.emailValidator])
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -41,5 +44,17 @@ export class AppComponent implements OnInit {
       return {'nameIsForbidden': true};
     }
     return null;
+  }
+
+  emailValidator(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
   }
 }
